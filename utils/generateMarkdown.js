71f109bbde1265953c46dfc48bc8bfee0b3b7a4b
AStandish <112442942+AStandish@users.noreply.github.com>
2022-12-05 16:51:49 -0500

    changed license language in generatemd.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,12 +2,12 @@ const fs = require("fs");
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (!license) {
-    return ``;
-  } else {
+  if (license) {
     return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(
       license
     )})`;
+  } else {
+    return ``;
   }
 }
 
@@ -28,11 +28,11 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (!license) {
-    return ``;
-  } else {
+  if (license) {
     return `## Licenses
     This design is covered under the ${license} license. To understand what this means, click the license button at the top.`;
+  } else {
+    return ``;
   }
 }
 
